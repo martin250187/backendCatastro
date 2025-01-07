cb63b2955869082051bfdccce9a906a1eb5fda6c
GIS Olavarria <gis@olavarria.gov.ar>
2025-01-07 10:33:30 -0300

    Modifico estructura

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // /api/index.js
-const express = require('express');
-const cors = require('cors');
-const { getData } = require('../src/controllers/dataController'); // Asegúrate de que el path sea correcto
+const express = require("express");
+const cors = require("cors");
+const database = require('../db');  // Asegúrate de que la ruta sea correcta
 
 const app = express();
 
@@ -12,7 +12,23 @@ app.use(express.json());
 app.use(cors());
 
 // Definir la ruta
-app.get('/api/data', getData);
+app.get("/api/data", async (req, res) => {
+  try {
+    const [parcelas] = await database.query(
+      "SELECT id, geom, nomencla, partida, nomenclatura, parcela, has_rafam as has from catastro.parcelas_rurales_wgs84"
+    );
+    const [rafam] = await database.query(
+      "SELECT nro_inmueble, par_catastral, resp_pago, tipo from catastro.inmuebles_rafam where tipo='RUR'"
+    );
 
-// Exportar la función para ser utilizada por Vercel
-module.exports = app;
+    res.json([parcelas, rafam]);
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    res.status(500).json({ error: "Error al obtener los datos" });
+  }
+});
+
+// Exportar la función como handler para Vercel
+module.exports = (req, res) => {
+  app(req, res);  // Llama a la instancia de Express y pasa los objetos `req` y `res`
+};
